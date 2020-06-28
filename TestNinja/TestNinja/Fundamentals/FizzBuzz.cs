40f7914f37de2e6dd8101b4917131c2359b924fe
jaco <jaco.kleynhans@icloud.com>
2020-06-29 00:25:55 +0200

    Dependency Injection

--- TestNinja/TestNinja/Fundamentals/FizzBuzz.cs
+++ TestNinja/TestNinja/Fundamentals/FizzBuzz.cs
@@ -1,4 +1,8 @@
-﻿namespace TestNinja.Fundamentals
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestNinja.Fundamentals
 {
     public class FizzBuzz
     {
@@ -14,6 +18,61 @@
                 return "Buzz";
 
             return number.ToString(); 
+        }
+
+        private const int _initialCharge = 50;
+    private const int _max = 100;
+    private const int _min = 0;
+        
+        public static int getBattery(List<int> events)
+        {
+            int charge = _initialCharge;
+            for (int i = 0; i < events.Count; i++)
+            {
+                charge = charge + events[i];
+                if (charge < _min)
+                {
+                    charge = 0;
+                }
+
+                if (charge > _max)
+                {
+                    charge = 100;
+                }
+            }
+
+            return charge;
+
+            /*
+             * var useList = events.Where(p => p < 0).ToList();
+        var chargeList = events.Where(p => p >= 0).ToList();
+        var max = new List<int>() {useList.Count, chargeList.Count}.Max();
+        int charge = _initialCharge;
+             
+        for (int i = 0; i < max; i++)
+        {
+            charge = charge + chargeList[i] != null ? chargeList[i] :0 - useList[i] != null ? useList[i] : 0;
+            if (charge < _min)
+            {
+                charge = 0;
+            }
+
+            if (charge > _max)
+            {
+                charge = 100;
+            }
+            
+        }
+
+        return charge;
+        */
+        }
+        
+        public static int longestSubarray(List<int> arr)
+        {
+            var max = arr.Max(p => p);
+            var subArray = arr.Where(p => p % max < 1);
+            return subArray.Count();
         }
     }
 }
--- TestNinja/TestNinja/Mocking/VideoService.cs
+++ TestNinja/TestNinja/Mocking/VideoService.cs
@@ -9,9 +9,10 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
+        public IFileReader FileReader { get; set; } = new FileReader();
         public string ReadVideoTitle()
         {
-            var str = File.ReadAllText("video.txt");
+            var str = FileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
